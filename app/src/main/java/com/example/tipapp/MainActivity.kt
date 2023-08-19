9dccf58c53c6fa67b64ce9b2aa359c8fd158e60e
Jorge Flores <flores.jorgeluis@gmail.com>
2023-08-19 13:06:22 -0600

    Slider composable part 2

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -22,10 +22,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -49,14 +51,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApp() {
                 //  Text(text = "Hello Again")
-                //   TopHeader()
+
                 MainContent()
+
             }
         }
     }
 }
 
-@Preview
+//@Preview
 @Composable
 fun TopHeader(totalPerPerson: Double = 0.0) {
 
@@ -89,16 +92,17 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 }
 
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun MainContent() {
 
+    Column(modifier = Modifier.padding(all = 12.dp)) {
+        BillForm() {
 
-    BillForm() {
+                billAmt ->
+            Log.d("AMT", "Main Content: $billAmt")
 
-            billAmt ->
-        Log.d("AMT", "Main Content: $billAmt")
-
+        }
     }
 
 
@@ -117,6 +121,9 @@ fun BillForm(
         mutableIntStateOf(2)
     }
 
+    var sliderPositionState by remember {
+        mutableFloatStateOf(0f)
+    }
 
     val totalBillState = remember {
         mutableStateOf("")
@@ -125,6 +132,8 @@ fun BillForm(
         totalBillState.value.trim().isNotEmpty()
     }
     val keyboardController = LocalSoftwareKeyboardController.current
+
+    TopHeader()
 
     Surface(
         modifier = Modifier
@@ -153,56 +162,81 @@ fun BillForm(
                 }
             )
 
-            if (validState) {
-                Row(modifier = Modifier.padding(3.dp), horizontalArrangement = Arrangement.Start) {
+            // if (validState) {
+            Row(modifier = Modifier.padding(3.dp), horizontalArrangement = Arrangement.Start) {
 
-                    Text(
-                        text = "Split", modifier = Modifier.align(
-                            alignment = Alignment.CenterVertically
-                        )
+                Text(
+                    text = "Split", modifier = Modifier.align(
+                        alignment = Alignment.CenterVertically
                     )
+                )
 
-                    Spacer(modifier = Modifier.width(120.dp))
+                Spacer(modifier = Modifier.width(120.dp))
 
-                    Row(
-                        modifier = Modifier.padding(horizontal = 3.dp),
-                        horizontalArrangement = Arrangement.End
-                    ) {
+                Row(
+                    modifier = Modifier.padding(horizontal = 3.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
 
-                        RoundedIconButton(
-                            imageVector = Icons.Default.Remove,
-                            onClick = {
-                                if (splitNumber>1)
+                    RoundedIconButton(
+                        imageVector = Icons.Default.Remove,
+                        onClick = {
+                            if (splitNumber > 1)
                                 splitNumber--
 
 
-                            })
+                        })
 
-                        Text(
-                            text = splitNumber.toString(),
-                            modifier = Modifier
-                                .align(Alignment.CenterVertically)
-                                .padding(start = 9.dp, end = 9.dp)
-                        )
+                    Text(
+                        text = splitNumber.toString(),
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 9.dp, end = 9.dp)
+                    )
 
-                        RoundedIconButton(
-                            imageVector = Icons.Default.Add,
-                            onClick = { splitNumber++ })
-
-                    }
-
+                    RoundedIconButton(
+                        imageVector = Icons.Default.Add,
+                        onClick = { splitNumber++ })
 
                 }
-            } else {
-                Box {
 
-                }
+
             }
+
+            //Tip Row
+            Row(modifier = Modifier.padding(horizontal = 3.dp, vertical = 12.dp)) {
+
+                Text(text = "Tip")
+                Spacer(modifier = Modifier.width(200.dp))
+                Text(text = "$33.00")
+            }
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(text = "33%")
+                Spacer(modifier = Modifier.height(14.dp))
+
+                //Slider
+                Slider(value = sliderPositionState, onValueChange = { newval ->
+                    sliderPositionState = newval
+                    Log.d("SLIDER", "Bill form $newval")
+
+                }, modifier = Modifier.padding(start = 16.dp, end = 16.dp), steps = 5)
+
+            }
+
+            /*  } else {
+                  Box {
+
+                  }
+              }*/
 
         }
     }
-
-
+    
 }
 
 
@@ -224,8 +258,8 @@ fun DefaultPreview() {
 
     TipAppTheme {
         MyApp {
-            TopHeader()
-            BillForm()
+
+            MainContent()
         }
     }
 
